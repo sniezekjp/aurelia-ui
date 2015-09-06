53e12995b2ed0b107243793b340dc9131fce62da
JP <jsniezek@crowdskout.com>
2015-09-06 13:25:35 -0400

    Updating quick

--- quick.sh
+++ quick.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+mkdir src/app/root/$1
 touch src/app/root/$1/$1.{ts,html,scss}
 echo "export class $2 {}" > src/app/root/$1/$1.ts
 echo "<template></template>" > src/app/root/$1/$1.html
