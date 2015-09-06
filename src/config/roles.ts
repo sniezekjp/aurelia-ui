1baa58c8d5eb236002f54a3ccb489bf1ca38faee
JP <jsniezek@crowdskout.com>
2015-09-06 14:07:06 -0400

    More configs

---
+++ src/config/roles.ts
@@ -0,0 +1,4 @@
+export const ADMIN = 1;
+export const MANAGER = 2;
+export const EDITOR = 3;
+export const VOLUNTEER = 4;
---
+++ src/config/sidebar.ts
@@ -0,0 +1,7 @@
+export var admin = {};
+export var manager = {};
+export var editor = {};
+export var volunteer = {};
+
+
+
