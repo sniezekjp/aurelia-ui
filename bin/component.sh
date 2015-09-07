f23110b6ffb37111705b3ca38d1609d1ac50d6c9
JP <jsniezek@crowdskout.com>
2015-09-07 11:05:13 -0400

    bin

--- bin/component.sh
+++ bin/component.sh
@@ -4,3 +4,4 @@ mkdir src/components/$1
 touch src/components/$1/$1.{ts,html,scss}
 echo "export class $2 {}" > src/components/$1/$1.ts
 echo "<template></template>" > src/components/$1/$1.html
+echo "component created"
--- bin/root.sh
+++ bin/root.sh
@@ -4,3 +4,4 @@ mkdir src/app/root/$1
 touch src/app/root/$1/$1.{ts,html,scss}
 echo "export class $2 {}" > src/app/root/$1/$1.ts
 echo "<template></template>" > src/app/root/$1/$1.html
+echo "root route created"
