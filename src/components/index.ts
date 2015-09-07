0a962d78d1c010f6290b587b43f33447cd93c226
JP <jsniezek@crowdskout.com>
2015-09-06 23:51:04 -0400

    Timepicker

--- src/components/index.ts
+++ src/components/index.ts
@@ -22,6 +22,7 @@ export function configure(config: FrameworkConfiguration) {
          'progress-bar',
          'signin',
          'tabs',
+         'timepicker',
          'tool-tip',
          'user-details',
          'weather'
